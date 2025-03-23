69730a6c708da8d7d9c5dd48f3d746daa43ec62f
pipitochka <aaakulov_3@edu.hse.ru>
2025-03-23 11:54:13 +0300

    changes in cell

--- Assets/_Source/_Core/Cell.cs
+++ Assets/_Source/_Core/Cell.cs
@@ -1,36 +1,69 @@
-using UnityEngine;
 using System;
+using TMPro;
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
-public class Cell
+public class Cell : MonoBehaviour
 {
-    public float X { get; private set; }
-    public float Y { get; private set; }
+    public int X { get; private set; }
+    public int Y {get; private set;}
+    
     public int Value {get; private set;}
     
-    public event Action<int> OnValueChanged;
-    public event Action<float, float> OnPositionChanged;
+    public int Points => Value == 0 ? 0 : (int)Mathf.Pow(2, Value);
+    
+    public bool IsEmpty => Value == 0;
 
-    public Cell(float x, float y, int value)
+    public const int MaxValue = 11;
+
+    public bool HasMerged { get; private set; }
+    
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    TextMeshProUGUI points;
+
+    public void SetValue(int x, int y, int value)
     {
         X = x;
         Y = y;
         Value = value;
+        
+        UpdateCell();
     }
 
-    public void UpdateValue(int value)
+    public void UpdateCell()
     {
-        Value = value;
-        OnValueChanged?.Invoke(Value);
+        points.text = IsEmpty ? String.Empty : Points.ToString();
+        image.color = ColorManager.Instance.CellColors[Value];
     }
 
-    public void UpdatePosition(float x, float y)
+    public void IncreaseValue()
     {
-        if (X != x || Y != y)
-        {
-            X = x;
-            Y = y;
-            OnPositionChanged?.Invoke(X, Y);
-        }
+        Value++;
+        HasMerged = true;
+        
+        GameControlller.Instance.AddPoints(Points);
+        
+        UpdateCell();
     }
-    
+
+    public void ResetFlags()
+    {
+        HasMerged = false;
+    }
+
+    public void MergeWithCell(Cell cell)
+    {
+        cell.IncreaseValue();
+        SetValue(X, Y, 0);
+        
+        UpdateCell();
+    }
+
+    public void MoveToCell(Cell cell)
+    {
+        cell.SetValue(cell.X, cell.Y, Value);
+        SetValue(X, Y, 0);
+    }
 }
